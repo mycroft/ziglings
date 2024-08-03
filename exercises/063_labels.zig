cad92d379a774119c61e4a498ae60bedebdd9ce1
Patrick Marie <pm@mkz.me>
2024-08-03 13:32:33 +0200

    063_labels

--- exercises/063_labels.zig
+++ exercises/063_labels.zig
@@ -102,6 +102,8 @@ pub fn main() void {
     // application!
     const wanted_ingredients = [_]u8{ 0, 3 }; // Chili, Cheese
 
+    const default_menu = menu[0];
+
     // Look at each Food on the menu...
     const meal = food_loop: for (menu) |food| {
 
@@ -128,8 +130,8 @@ pub fn main() void {
         // wanted for this Food.
         //
         // Please return this Food from the loop.
-        break;
-    };
+        break food;
+    } else default_menu;
     // ^ Oops! We forgot to return Mac & Cheese as the default
     // Food when the requested ingredients aren't found.
 
